15995fabe80c2f357f16da049efe055a52d341e0
NoahAdeyeye <noah2adeyeye@gmail.com>
2024-10-15 15:01:36 -0400

    Added new new package in my github repository for midterm

---
+++ src/org/howard/edu/lsp/midterm/HelloWorld.java
@@ -0,0 +1,13 @@
+
+/**
+*  Name: Oluwaseun Noah Adeyeye
+*/
+package org.howard.edu.lsp.midterm;
+
+public class HelloWorld {
+    
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
+

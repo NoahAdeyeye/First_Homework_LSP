c9538dc0649512772e0ccc086d895e4963be99be
NoahAdeyeye <noah2adeyeye@gmail.com>
2024-12-05 20:26:58 -0500

    Small change for final Exam, earlier removed comments for clairity, added back

--- src/org/howard/edu/lsp/finalexam/question1/MathUtils.java
+++ src/org/howard/edu/lsp/finalexam/question1/MathUtils.java
@@ -1,6 +1,10 @@
 package org.howard.edu.lsp.finalexam.question1;
 
 public class MathUtils {
+    /**
+     * Returns the factorial of a given non-negative integer.
+     * If the input is negative, throws IllegalArgumentException.
+     */
     public int factorial(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Number must be non-negative");
@@ -11,6 +15,10 @@ public class MathUtils {
         return n * factorial(n - 1);
     }
 
+    /**
+     * Returns true if the given number is a prime number, 
+     * false otherwise.
+     */
     public boolean isPrime(int n) {
         if (n <= 1) return false;
         for (int i = 2; i <= Math.sqrt(n); i++) {
@@ -21,6 +29,12 @@ public class MathUtils {
         return true;
     }
 
+    /**
+     * Returns the greatest common divisor (GCD) of two 
+     * integers using the Euclidean algorithm. If either input 
+     * is zero, returns the absolute value of the non-zero input.
+     * If both inputs are zero, throws IllegalArgumentException.
+     */
     public int gcd(int a, int b) {
         if (a == 0 && b == 0) {
             throw new IllegalArgumentException("Both numbers cannot be zero");
